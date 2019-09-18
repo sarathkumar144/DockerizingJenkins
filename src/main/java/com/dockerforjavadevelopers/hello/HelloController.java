cccd5393c448a4dc78713d24ff88cbaad44bb4ba
sarathkumar144 <sarathkumar14@gmail.com>
2019-09-18 11:22:46 -0500

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -2,7 +2,8 @@ package com.dockerforjavadevelopers.hello;
 
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloController {

c78ddd2ae81a5b2f5e0ea0a2a310552165d6f22d
sarathkumar144 <sarathkumar14@gmail.com>
2019-09-18 11:35:58 -0500

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     
     @GetMapping("/greet/{name}")
     public String index(@PathVariable String name) {
-        return "Hi!!  " + name;;
+        return "Hi!!  " + name;
     }
     
 }
